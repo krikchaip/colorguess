32ed0bf2e1bc95cd197e1728406e5b868174c656
RenniW <krikchai.p@gmail.com>
2017-03-04 02:25:21 +0700

    เปลี่ยนใหม่เป็นแบบ OOP

--- setup.js
+++ setup.js
@@ -1,100 +1,75 @@
-/*----- Global Variables -----*/
-// enter button id
-var startmode = 'mode-easy';
+//+++ Global Functions +++//
+/**
+ * Returns a string of RGB in 'rgb(a,b,c)' format.
+ */
+function getRGB () {
+	var f = function () {return Math.floor(Math.random() * 256);}
+	return 'rgb' + '(' + f() + ',' + f() + ',' + f() + ')';
+}
 
-/*----- Independent Functions -----*/
-	function rgb_generator(fn) {
-		if (fn == undefined) {
-			fn = function () {
-				var f = function () { return Math.floor(Math.random() * 256); }
-				return 'rgb'+'('+f()+','+f()+','+f()+')';  
-			};
-		}
-		return fn();
-	}
+//+++ Classes +++//
+// TODO: hide-when-click function
+function Tiles () {
+	var container = document.querySelector('.container');
+	var tilesList = container.children;
 
-/*------------------------------*/
-var Tiles = {
-	'item' : undefined,
-	'container' : document.querySelector('.container'),
-	'set_to' : function (count) {
-		if (this.container.childElementCount !== count) {
-			this.container.innerHTML = '';
+	this.get = function () {return tilesList;};
+	this.getContainer = function () {return container;}
+	this.setAndUpdate = function (value, update) {
+		if (container.childElementCount !== value) {
 			var inject = '';
-			for (var i = 0; i < count; i++) {
+			for (var i = 0; i < value; i++) {
 				inject += '<div class="color-box" id="box'+(i+1)+'"></div>';
 			}
-			this.container.innerHTML = inject;
-
-			delete this.item;
-			this.item = this.container.children;
-		}
-	},
-	'set_color' : function () {
-		for (var i = 0; i < this.item.length; i++) {
-			this.item[i].style.backgroundColor = rgb_generator();
-		}
-	}
-};
-
-var Answer = {
-	'value' : undefined, 
-	'object' : document.getElementById('answer'),
-	'pick' : function () {
-		return Tiles.item[Math.floor(Math.random() * Tiles.item.length)].style.backgroundColor;
-	},
-	'update' : function () {
-		this.value = this.pick();
-		this.object.textContent = this.value;
-	}
-};
-
-var Button = {
-	'button' : {},
-	'nowclick' : function (button_name) {
-		if (button_name != 'newgame') {
-			this.initiate();
-			this.button[button_name].setActive(false);
-		}
-		console.log(button_name);
-		return button_name;
-	},
-	'make_button' : function (name, event) {
-		// create Button name = (name)
-		this.button[name] = {
-			'obj' : document.getElementById(name),
-			'action' : [
-				event,
-				function () {
-					Button.nowclick(name);
-					Tiles.set_color();
-					Answer.update();
-				}
-			],
-			'setActive' : function (option) {
-				if (option)
-					for (var i = 0; i < this.action.length; i++)
-						this.obj.addEventListener('click', this.action[i]);
-				else 
-					for (var i = 0; i < this.action.length; i++)
-						this.obj.removeEventListener('click', this.action[i]);
+			container.innerHTML = inject;
+			for (var i = 0; i < tilesList.length; i++) {
+				tilesList[i].style.backgroundColor = getRGB();
 			}
-		}; // end this[name]
-	}, // end 'make_button'
-	'initiate' : function (startmode) {
-		if (startmode == undefined) startmode = 'mode-easy';
-		for (key in this.button)
-			this.button[key].setActive(true);
-	}
-};
+			update(tilesList);
+		}
+	};
+	this.resetColor = function () {
+		for (var i = 0; i < tilesList.length; i++) {
+			tilesList[i].style.backgroundColor = getRGB();
+		}
+	};
+}
 
-/* Execution Steps */
-	(function () {
-		Button.make_button('newgame', undefined);
-		Button.make_button('mode-easy', function () { Tiles.set_to(3); });
-		Button.make_button('mode-normal', function () { Tiles.set_to(6); });
-		Button.make_button('mode-hard', function () { Tiles.set_to(9); });
+function Answer () {
+	var value = undefined;
+	var display = document.getElementById('answer');
 
-		Button.initiate(startmode);
-		Button.button[startmode].obj.click();
-	})();
+	this.get = function () {return value;};
+	this.pickFrom = function (item) {
+		value = item[Math.floor(Math.random() * item.length)].style.backgroundColor;
+		display.textContent = value;
+	};
+}
+
+//+++ Setup +++//
+// Create game objects //
+var tiles = new Tiles();
+var answer = new Answer();
+
+// Create game buttons //
+var newgameButton = document.getElementById('newgame');
+newgameButton.addEventListener('click', function () {
+	tiles.resetColor();
+	answer.pickFrom(tiles.get());
+});
+var easyModeButton = document.getElementById('mode-easy');
+easyModeButton.addEventListener('click', function () {
+	tiles.setAndUpdate(6, answer.pickFrom);
+});
+var normalModeButton = document.getElementById('mode-normal');
+normalModeButton.addEventListener('click', function () {
+	tiles.setAndUpdate(9, answer.pickFrom);
+});
+var hardModeButton = document.getElementById('mode-hard');
+hardModeButton.addEventListener('click', function () {
+	tiles.setAndUpdate(12, answer.pickFrom);
+});
+
+// Initialize //
+easyModeButton.click();
+newgameButton.click();
